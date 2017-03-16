17bbc72f9e7314e89180166ec90ce6f5d25fdf2e
Maxim Rysevets <root@effcore.com>
2017-03-16 13:09:55 +0300

    Initial commit.

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace effectivecore {
+
+  const format_date     = 'Y-m-d';
+  const format_time     = 'H:i:s';
+  const format_datetime = 'Y-m-d H:i:s';
+  const dir_root        = __DIR__;
+  const dir_cache       = __DIR__.'/cache';
+  const dir_modules     = __DIR__.'/modules';
+  const dir_settings    = __DIR__.'/settings';
+  const nl              = "\n";
+
+  require_once('modules/core/classes/class_abstract--core.php');
+  core::init();
+
+}
---
+++ modules/core/classes/class_abstract--timer.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace effectivecore {
+          abstract class timer {
+
+  static $data = [];
+
+  static function tap($name) {
+    static::$data[$name][] = microtime();
+  }
+
+  static function get_period($name, $a, $b) {
+    return round(static::$data[$name][$b] - static::$data[$name][$a], 6);
+  }
+
+}}
