84c4f51d2b599a14ffa79cdfcbd02e2c537b3f0b
Maxim Rysevets <root@effcore.com>
2017-04-02 14:41:43 +0300

    Iteration #209

--- index.php
+++ index.php
@@ -5,10 +5,10 @@ namespace effectivecore {
   const format_date     = 'Y-m-d';
   const format_time     = 'H:i:s';
   const format_datetime = 'Y-m-d H:i:s';
-  const dir_root        = __DIR__;
-  const dir_cache       = __DIR__.'/cache';
-  const dir_modules     = __DIR__.'/modules';
-  const dir_settings    = __DIR__.'/settings';
+  const dir_root        = __DIR__.'/';
+  const dir_cache       = __DIR__.'/cache/';
+  const dir_modules     = __DIR__.'/modules/';
+  const dir_settings    = __DIR__.'/settings/';
   const nl              = "\n";
   const br              = "<br/>";
 
