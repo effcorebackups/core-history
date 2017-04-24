726cf1d0d9234724a1dc758d9fcbc9e3013fcebb
Maxim Rysevets <root@effcore.com>
2017-04-24 20:57:06 +0300

    Iteration #263-35

--- modules/core/classes/gl--factory--settings.php
+++ modules/core/classes/gl--factory--settings.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace effectivecore {
+          use \effectivecore\files_factory as files;
           abstract class settings_factory {
 
   static $data;
@@ -13,8 +14,8 @@ namespace effectivecore {
 
   static protected function _update() {
     $parse = [];
-    $files = files_factory::get_all(dir_modules, '%^.*\._s$%') +
-             files_factory::get_all(dir_settings, '%^.*\._s$%');
+    $files = files::get_all(dir_modules, '%^.*\._s$%') +
+             files::get_all(dir_settings, '%^.*\._s$%');
     $modules = [];
     foreach ($files as $c_file) {
       if ($c_file->get_file_full() == 'module._s') {
