84ac2b62040416f19c4ec3123a40c1611536faeb
Maxim Rysevets <root@effcore.com>
2017-06-12 12:44:29 +0300

    Iteration #302-75

---
+++ dynamic/changes.php
@@ -0,0 +1,24 @@
+<?php 
+
+namespace effectivecore { # ARRAY[type][scope]...
+
+  use \effectivecore\storage_instance_s as settings;
+
+  settings::$data_orig['changes_dynamic']['storage']['test_insert_arr'] = new \stdClass();
+  settings::$data_orig['changes_dynamic']['storage']['test_insert_arr']->action = 'insert';
+  settings::$data_orig['changes_dynamic']['storage']['test_insert_arr']->npath = 'test_changes/storage/test_arr';
+  settings::$data_orig['changes_dynamic']['storage']['test_insert_arr']->value['item_4'] = 'a4';
+  settings::$data_orig['changes_dynamic']['storage']['test_insert_arr']->value['item_5'] = 'a5';
+  settings::$data_orig['changes_dynamic']['storage']['test_insert_arr']->value['item_6'] = 'a6';
+  settings::$data_orig['changes_dynamic']['storage']['test_insert_arr']->module_id = 'storage';
+  settings::$data_orig['changes_dynamic']['storage']['test_update_arr'] = new \stdClass();
+  settings::$data_orig['changes_dynamic']['storage']['test_update_arr']->action = 'update';
+  settings::$data_orig['changes_dynamic']['storage']['test_update_arr']->npath = 'test_changes/storage/test_arr/item_1';
+  settings::$data_orig['changes_dynamic']['storage']['test_update_arr']->value = 'a1 modified';
+  settings::$data_orig['changes_dynamic']['storage']['test_update_arr']->module_id = 'storage';
+  settings::$data_orig['changes_dynamic']['storage']['test_delete_arr'] = new \stdClass();
+  settings::$data_orig['changes_dynamic']['storage']['test_delete_arr']->action = 'delete';
+  settings::$data_orig['changes_dynamic']['storage']['test_delete_arr']->npath = 'test_changes/storage/test_arr/item_2';
+  settings::$data_orig['changes_dynamic']['storage']['test_delete_arr']->module_id = 'storage';
+
+}
