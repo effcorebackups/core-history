a325a09d941ec963b34abd6011775c4c934e5f2a
Maxim Rysevets <root@effcore.com>
2017-06-13 15:52:12 +0300

    Iteration #302-88

--- dynamic/changes.php
+++ dynamic/changes.php
@@ -4,18 +4,18 @@ namespace effectivecore { # ARRAY[type][scope]...
 
   use \effectivecore\storage_instance_s as settings;
 
-  settings::$data_orig['changes_dynamic']['storage']['test_insert_arr'] = new \stdClass();
-  settings::$data_orig['changes_dynamic']['storage']['test_insert_arr']->action = 'insert';
-  settings::$data_orig['changes_dynamic']['storage']['test_insert_arr']->npath = 'test_changes/storage/test_arr';
-  settings::$data_orig['changes_dynamic']['storage']['test_insert_arr']->value['item_4'] = 'a4';
-  settings::$data_orig['changes_dynamic']['storage']['test_insert_arr']->value['item_5'] = 'a5';
-  settings::$data_orig['changes_dynamic']['storage']['test_insert_arr']->value['item_6'] = 'a6';
-  settings::$data_orig['changes_dynamic']['storage']['test_update_arr'] = new \stdClass();
-  settings::$data_orig['changes_dynamic']['storage']['test_update_arr']->action = 'update';
-  settings::$data_orig['changes_dynamic']['storage']['test_update_arr']->npath = 'test_changes/storage/test_arr/item_1';
-  settings::$data_orig['changes_dynamic']['storage']['test_update_arr']->value = 'a1 modified';
-  settings::$data_orig['changes_dynamic']['storage']['test_delete_arr'] = new \stdClass();
-  settings::$data_orig['changes_dynamic']['storage']['test_delete_arr']->action = 'delete';
-  settings::$data_orig['changes_dynamic']['storage']['test_delete_arr']->npath = 'test_changes/storage/test_arr/item_2';
+  settings::$changes_dynamic['changes']['storage']['test_insert_arr'] = new \stdClass();
+  settings::$changes_dynamic['changes']['storage']['test_insert_arr']->action = 'insert';
+  settings::$changes_dynamic['changes']['storage']['test_insert_arr']->npath = 'test_changes/storage/test_arr';
+  settings::$changes_dynamic['changes']['storage']['test_insert_arr']->value['item_4'] = 'a4';
+  settings::$changes_dynamic['changes']['storage']['test_insert_arr']->value['item_5'] = 'a5';
+  settings::$changes_dynamic['changes']['storage']['test_insert_arr']->value['item_6'] = 'a6';
+  settings::$changes_dynamic['changes']['storage']['test_update_arr'] = new \stdClass();
+  settings::$changes_dynamic['changes']['storage']['test_update_arr']->action = 'update';
+  settings::$changes_dynamic['changes']['storage']['test_update_arr']->npath = 'test_changes/storage/test_arr/item_1';
+  settings::$changes_dynamic['changes']['storage']['test_update_arr']->value = 'a1 modified';
+  settings::$changes_dynamic['changes']['storage']['test_delete_arr'] = new \stdClass();
+  settings::$changes_dynamic['changes']['storage']['test_delete_arr']->action = 'delete';
+  settings::$changes_dynamic['changes']['storage']['test_delete_arr']->npath = 'test_changes/storage/test_arr/item_2';
 
 }
