a7033c4bbfb1f1fe0356b528b0b4d90d8eb51965
Maxim Rysevets <root@effcore.com>
2017-06-21 14:42:22 +0300

    Iteration #304-17

--- dynamic/changes.php
+++ dynamic/changes.php
@@ -7,6 +7,6 @@ namespace effectivecore { # ARRAY[type][scope]...
   settings::$changes_dynamic['changes']['page']['update_decoration_page_background_color'] = new \stdClass();
   settings::$changes_dynamic['changes']['page']['update_decoration_page_background_color']->action = 'update';
   settings::$changes_dynamic['changes']['page']['update_decoration_page_background_color']->npath = 'decoration/page/background_color';
-  settings::$changes_dynamic['changes']['page']['update_decoration_page_background_color']->value = 'color_yellowgreen';
+  settings::$changes_dynamic['changes']['page']['update_decoration_page_background_color']->value = 'color_orange';
 
 }
