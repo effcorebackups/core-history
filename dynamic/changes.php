9e19b5da965f4baada7f342c7a982782f7526aa1
Maxim Rysevets <root@effcore.com>
2017-07-09 17:23:07 +0300

    Iteration #304-96

--- dynamic/changes.php
+++ dynamic/changes.php
@@ -4,7 +4,8 @@ namespace effectivecore { # ARRAY[type][scope]...
 
   use \effectivecore\storage_instance_s as settings;
 
-  settings::$changes_dynamic['changes']['page']['update']['decoration/page/color'] = 'color_black';
-  settings::$changes_dynamic['changes']['page']['update']['decoration/page/color_bg'] = 'color_original_blue';
+  settings::$changes_dynamic['changes']['page'] = new \stdClass();
+  settings::$changes_dynamic['changes']['page']->update['decoration/page/color'] = 'color_black';
+  settings::$changes_dynamic['changes']['page']->update['decoration/page/color_bg'] = 'color_original_blue';
 
 }
