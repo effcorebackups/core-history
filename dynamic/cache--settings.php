c0b2aa35332df6d0c71e08dac2d622716306eb63
Maxim Rysevets <root@effcore.com>
2017-07-13 12:23:15 +0300

    Iteration #304-99

--- dynamic/cache--settings.php
+++ dynamic/cache--settings.php
@@ -4,7 +4,7 @@ namespace effectivecore { # ARRAY[type][scope]...
 
   use \effectivecore\storage_instance_s as settings;
 
-  settings::$data['_created'] = '2017-07-13 12:12:39';
+  settings::$data['_created'] = '2017-07-13 12:22:46';
   settings::$data['file_types']['core']['php'] = new \stdClass();
   settings::$data['file_types']['core']['php']->protected = 1;
   settings::$data['file_types']['core']['php']->headers['Content-type'] = 'text/html';
