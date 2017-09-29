c7150ab62a1cbd5d25f885217551b52a9a9d1364
Maxim Rysevets <root@effcore.com>
2017-09-29 12:00:11 +0300

    Iteration #311-142-13

--- dynamic/cache--settings.php
+++ dynamic/cache--settings.php
@@ -2,7 +2,7 @@
 
 namespace effectivecore { # cache for settings
 
-  caches_factory::$data['settings']['_created'] = '2017-09-29 10:08:03';
+  caches_factory::$data['settings']['_created'] = '2017-09-29 11:47:18';
   caches_factory::$data['settings']['module']['core'] = new \stdClass();
   caches_factory::$data['settings']['module']['core']->id = 'core';
   caches_factory::$data['settings']['module']['core']->title = 'Core';
