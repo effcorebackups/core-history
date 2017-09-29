a355c6a945cbffa8c8ded5389457268ee5ef084a
Maxim Rysevets <root@effcore.com>
2017-09-29 10:49:14 +0300

    Iteration #311-142-10

---
+++ dynamic/data--changes.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace effectivecore { # data for changes
+
+  dynamic_factory::$data['changes']['page'] = new \stdClass();
+  dynamic_factory::$data['changes']['page']->update['decoration/page/color_id'] = 'color_black';
+  dynamic_factory::$data['changes']['page']->update['decoration/page/color_bg_id'] = 'color_original_blue';
+
+}
