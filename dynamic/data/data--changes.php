c15786bd6bcf40a8992aae077dcef965dd060cd4
Maxim Rysevets <root@effcore.com>
2017-10-10 18:59:56 +0300

    Iteration #311-179-14

--- dynamic/data/data--changes.php
+++ dynamic/data/data--changes.php
@@ -2,10 +2,7 @@
 
 namespace effectivecore { # data for changes
 
-  dynamic_factory::$info['changes']['build'] = '2017-10-10 18:18:56';
-  dynamic_factory::$data['changes']['page'] = new \stdClass();
-  dynamic_factory::$data['changes']['page']->update['decoration/page/color_id'] = 'color_black';
-  dynamic_factory::$data['changes']['page']->update['decoration/page/color_bg_id'] = 'color_original_blue';
+  dynamic_factory::$info['changes']['build'] = '2017-10-10 18:58:31';
   dynamic_factory::$data['changes']['core'] = new \stdClass();
   dynamic_factory::$data['changes']['core']->insert['storages/storage/storage_sql_dpo'] = new \stdClass();
   dynamic_factory::$data['changes']['core']->insert['storages/storage/storage_sql_dpo']->driver = 'mysql';
