b8f376f8d115db50a492551071eefaa50f7741a9
Maxim Rysevets <root@effcore.com>
2017-10-10 18:19:44 +0300

    Iteration #311-179-10

--- dynamic/cache/cache--settings--storages.php
+++ dynamic/cache/cache--settings--storages.php
@@ -4,6 +4,11 @@ namespace effectivecore { # cache for settings--storages
 
   cache_factory::$data['settings--storages']['storage']['storage_sql_dpo'] = new \effectivecore\storage_pdo();
   cache_factory::$data['settings--storages']['storage']['storage_sql_dpo']->id = 'db';
+  cache_factory::$data['settings--storages']['storage']['storage_sql_dpo']->database_name = 'main';
+  cache_factory::$data['settings--storages']['storage']['storage_sql_dpo']->host_name = '127.0.0.1';
+  cache_factory::$data['settings--storages']['storage']['storage_sql_dpo']->user_name = 'root';
+  cache_factory::$data['settings--storages']['storage']['storage_sql_dpo']->password = '123';
+  cache_factory::$data['settings--storages']['storage']['storage_sql_dpo']->driver = 'mysql';
   cache_factory::$data['settings--storages']['storage']['storage_nosql_settings'] = new \effectivecore\storage_settings();
   cache_factory::$data['settings--storages']['storage']['storage_nosql_settings']->id = 'settings';
 
--- dynamic/data/data--changes.php
+++ dynamic/data/data--changes.php
@@ -2,9 +2,16 @@
 
 namespace effectivecore { # data for changes
 
-  dynamic_factory::$info['changes']['build'] = '2017-10-02 10:51:07';
+  dynamic_factory::$info['changes']['build'] = '2017-10-10 18:18:56';
   dynamic_factory::$data['changes']['page'] = new \stdClass();
   dynamic_factory::$data['changes']['page']->update['decoration/page/color_id'] = 'color_black';
   dynamic_factory::$data['changes']['page']->update['decoration/page/color_bg_id'] = 'color_original_blue';
+  dynamic_factory::$data['changes']['core'] = new \stdClass();
+  dynamic_factory::$data['changes']['core']->insert['storages/storage/storage_sql_dpo'] = new \stdClass();
+  dynamic_factory::$data['changes']['core']->insert['storages/storage/storage_sql_dpo']->driver = 'mysql';
+  dynamic_factory::$data['changes']['core']->insert['storages/storage/storage_sql_dpo']->host_name = '127.0.0.1';
+  dynamic_factory::$data['changes']['core']->insert['storages/storage/storage_sql_dpo']->database_name = 'main';
+  dynamic_factory::$data['changes']['core']->insert['storages/storage/storage_sql_dpo']->user_name = 'root';
+  dynamic_factory::$data['changes']['core']->insert['storages/storage/storage_sql_dpo']->password = '123';
 
 }
