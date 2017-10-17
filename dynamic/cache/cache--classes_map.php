cece5087f3c2f0ee435f47d2ae023bd58f354950
Maxim Rysevets <root@effcore.com>
2017-10-17 12:53:40 +0300

    Iteration #311-184-41

--- dynamic/cache/cache--classes_map.php
+++ dynamic/cache/cache--classes_map.php
@@ -2,7 +2,7 @@
 
 namespace effectivecore { # cache for classes_map
 
-  cache_factory::$info['classes_map']['build'] = '2017-10-17 12:39:44';
+  cache_factory::$info['classes_map']['build'] = '2017-10-17 12:53:27';
   cache_factory::$data['classes_map']['effectivecore\file'] = new \stdClass();
   cache_factory::$data['classes_map']['effectivecore\file']->type = 'class';
   cache_factory::$data['classes_map']['effectivecore\file']->namespace = 'effectivecore';
--- dynamic/cache/cache--settings_original.php
+++ dynamic/cache/cache--settings_original.php
@@ -2,7 +2,7 @@
 
 namespace effectivecore { # cache for settings_original
 
-  cache_factory::$info['settings_original']['build'] = '2017-10-17 12:39:44';
+  cache_factory::$info['settings_original']['build'] = '2017-10-17 12:53:27';
   cache_factory::$data['settings_original']['module']['core'] = new \stdClass();
   cache_factory::$data['settings_original']['module']['core']->id = 'core';
   cache_factory::$data['settings_original']['module']['core']->title = 'Core';
