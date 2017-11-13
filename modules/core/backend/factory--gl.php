c9d8ce1a4ed9554fad1a2676022d56d6a2c7a59d
Maxim Rysevets <root@effcore.com>
2017-11-13 15:56:14 +0300

    Iteration #311-248

--- modules/core/backend/factory--gl.php
+++ modules/core/backend/factory--gl.php
@@ -225,7 +225,7 @@ namespace effectivecore {
     return sha1($data);
   }
 
-  function binstr2hexstr($binstr) {
+  static function binstr2hexstr($binstr) {
     $hexstr = '';
     foreach (str_split($binstr, 8) as $c_chunk) {
       $hexstr.= str_pad(base_convert(str_pad($c_chunk, 8, '0'), 2, 16), 2, '0', STR_PAD_LEFT);
@@ -233,7 +233,7 @@ namespace effectivecore {
     return $hexstr;
   }
 
-  function hexstr2binstr($hexstr) {
+  static function hexstr2binstr($hexstr) {
     $binstr = '';
     foreach (str_split($hexstr, 2) as $c_chunk) {
       $binstr.= str_pad(base_convert($c_chunk, 16, 2), 8, '0', STR_PAD_LEFT);
