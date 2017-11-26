6ad7affbbf86da5df1b40caab26a844a80e9d37a
Maxim Rysevets <root@effcore.com>
2017-11-26 15:42:29 +0300

    Iteration #311-289-19

--- dynamic/data/data--changes.php
+++ dynamic/data/data--changes.php
@@ -2,7 +2,7 @@
 
 namespace effectivecore { # data for changes
 
-  dynamic_factory::$info['changes']['build'] = '2017-11-26 11:18:11';
+  dynamic_factory::$info['changes']['build'] = '2017-11-26 12:41:13';
   dynamic_factory::$data['changes']['core'] = new \stdClass();
   dynamic_factory::$data['changes']['core']->insert['storages/storage/storage_sql_dpo'] = new \stdClass();
   dynamic_factory::$data['changes']['core']->insert['storages/storage/storage_sql_dpo']->driver = 'mysql';
