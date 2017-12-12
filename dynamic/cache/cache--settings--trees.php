29c3c71045072a6ead7a09ea874bd626a4a88a1a
Maxim Rysevets <root@effcore.com>
2017-12-12 14:58:09 +0300

    Iteration #311-392-2

--- dynamic/cache/cache--settings--trees.php
+++ dynamic/cache/cache--settings--trees.php
@@ -7,10 +7,15 @@ namespace effectivecore { # cache for settings--trees
   cache::$data['settings--trees']['core']['main']->title = 'Main menu';
   cache::$data['settings--trees']['core']['main']->attributes['class']['main'] = 'main';
   cache::$data['settings--trees']['core']['main']->weight = 2;
-  cache::$data['settings--trees']['user']['user'] = new \effectivecore\tree();
-  cache::$data['settings--trees']['user']['user']->id = 'user';
-  cache::$data['settings--trees']['user']['user']->title = 'User menu';
-  cache::$data['settings--trees']['user']['user']->attributes['class']['user'] = 'user';
-  cache::$data['settings--trees']['user']['user']->weight = 1;
+  cache::$data['settings--trees']['user']['user_anonymous'] = new \effectivecore\tree();
+  cache::$data['settings--trees']['user']['user_anonymous']->id = 'user_anonymous';
+  cache::$data['settings--trees']['user']['user_anonymous']->title = 'User anonymous menu';
+  cache::$data['settings--trees']['user']['user_anonymous']->attributes['class']['user'] = 'user';
+  cache::$data['settings--trees']['user']['user_anonymous']->attributes['class']['user-anonymous'] = 'user-anonymous';
+  cache::$data['settings--trees']['user']['user_logged_in'] = new \effectivecore\tree();
+  cache::$data['settings--trees']['user']['user_logged_in']->id = 'user_logged_in';
+  cache::$data['settings--trees']['user']['user_logged_in']->title = 'User authenticated menu';
+  cache::$data['settings--trees']['user']['user_logged_in']->attributes['class']['user'] = 'user';
+  cache::$data['settings--trees']['user']['user_logged_in']->attributes['class']['user-logged_in'] = 'user-logged_in';
 
 }
