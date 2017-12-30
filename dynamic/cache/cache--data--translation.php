272cdd084f374f653fd55984ae6489d56291676a
Maxim Rysevets <root@effcore.com>
2017-12-30 21:44:53 +0300

    Iteration #311-426-69

--- dynamic/cache/cache--data--translation.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace effectivecore { # cache for data--translation
-
-  cache::$data['data--translation']['locales']['ru'] = new \effectivecore\different_cache();
-  cache::$data['data--translation']['locales']['ru']->cache_name = 'data--translation-locales-ru';
-  cache::$data['data--translation']['locales']['ru']->code = 'ru';
-
-}
