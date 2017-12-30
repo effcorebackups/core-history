e92a47f65b3de14ddcbe6a2d9414229a5a891c6b
Maxim Rysevets <root@effcore.com>
2017-12-30 21:33:56 +0300

    Iteration #311-426-67

--- dynamic/cache/cache--classes_map.php
+++ dynamic/cache/cache--classes_map.php
@@ -2,7 +2,7 @@
 
 namespace effectivecore { # cache for classes_map
 
-  cache::$info['classes_map']['build'] = '2017-12-30 18:23:46';
+  cache::$info['classes_map']['build'] = '2017-12-30 18:32:50';
   cache::$data['classes_map']['effectivecore\file'] = new \stdClass();
   cache::$data['classes_map']['effectivecore\file']->type = 'class';
   cache::$data['classes_map']['effectivecore\file']->namespace = 'effectivecore';
---
+++ dynamic/cache/cache--data--entities.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace effectivecore { # cache for data--entities
+
+  cache::$data['data--entities']['develop']['demo_types'] = new \effectivecore\different_cache();
+  cache::$data['data--entities']['develop']['demo_types']->cache_name = 'data--entities-develop-demo_types';
+  cache::$data['data--entities']['develop']['demo_types']->name = 'demo_types';
+  cache::$data['data--entities']['develop']['demo_types']->storage_id = 'main';
+  cache::$data['data--entities']['develop']['demo_autoincrement'] = new \effectivecore\different_cache();
+  cache::$data['data--entities']['develop']['demo_autoincrement']->cache_name = 'data--entities-develop-demo_autoincrement';
+  cache::$data['data--entities']['develop']['demo_autoincrement']->name = 'demo_autoincrement';
+  cache::$data['data--entities']['develop']['demo_autoincrement']->storage_id = 'main';
+  cache::$data['data--entities']['develop']['demo_constraints'] = new \effectivecore\different_cache();
+  cache::$data['data--entities']['develop']['demo_constraints']->cache_name = 'data--entities-develop-demo_constraints';
+  cache::$data['data--entities']['develop']['demo_constraints']->name = 'demo_constraints';
+  cache::$data['data--entities']['develop']['demo_constraints']->storage_id = 'main';
+  cache::$data['data--entities']['develop']['demo_indexes'] = new \effectivecore\different_cache();
+  cache::$data['data--entities']['develop']['demo_indexes']->cache_name = 'data--entities-develop-demo_indexes';
+  cache::$data['data--entities']['develop']['demo_indexes']->name = 'demo_indexes';
+  cache::$data['data--entities']['develop']['demo_indexes']->storage_id = 'main';
+  cache::$data['data--entities']['page']['captcha'] = new \effectivecore\different_cache();
+  cache::$data['data--entities']['page']['captcha']->cache_name = 'data--entities-page-captcha';
+  cache::$data['data--entities']['page']['captcha']->name = 'captcha';
+  cache::$data['data--entities']['page']['captcha']->storage_id = 'main';
+  cache::$data['data--entities']['tree']['tree'] = new \effectivecore\different_cache();
+  cache::$data['data--entities']['tree']['tree']->cache_name = 'data--entities-tree-tree';
+  cache::$data['data--entities']['tree']['tree']->name = 'tree';
+  cache::$data['data--entities']['tree']['tree']->storage_id = 'main';
+  cache::$data['data--entities']['tree']['tree_item'] = new \effectivecore\different_cache();
+  cache::$data['data--entities']['tree']['tree_item']->cache_name = 'data--entities-tree-tree_item';
+  cache::$data['data--entities']['tree']['tree_item']->name = 'tree_item';
+  cache::$data['data--entities']['tree']['tree_item']->storage_id = 'main';
+  cache::$data['data--entities']['user']['user'] = new \effectivecore\different_cache();
+  cache::$data['data--entities']['user']['user']->cache_name = 'data--entities-user-user';
+  cache::$data['data--entities']['user']['user']->name = 'user';
+  cache::$data['data--entities']['user']['user']->storage_id = 'main';
+  cache::$data['data--entities']['user']['session'] = new \effectivecore\different_cache();
+  cache::$data['data--entities']['user']['session']->cache_name = 'data--entities-user-session';
+  cache::$data['data--entities']['user']['session']->name = 'session';
+  cache::$data['data--entities']['user']['session']->storage_id = 'main';
+  cache::$data['data--entities']['user']['role'] = new \effectivecore\different_cache();
+  cache::$data['data--entities']['user']['role']->cache_name = 'data--entities-user-role';
+  cache::$data['data--entities']['user']['role']->name = 'role';
+  cache::$data['data--entities']['user']['role']->storage_id = 'main';
+  cache::$data['data--entities']['user']['permission'] = new \effectivecore\different_cache();
+  cache::$data['data--entities']['user']['permission']->cache_name = 'data--entities-user-permission';
+  cache::$data['data--entities']['user']['permission']->name = 'permission';
+  cache::$data['data--entities']['user']['permission']->storage_id = 'main';
+  cache::$data['data--entities']['user']['relation_role_ws_user'] = new \effectivecore\different_cache();
+  cache::$data['data--entities']['user']['relation_role_ws_user']->cache_name = 'data--entities-user-relation_role_ws_user';
+  cache::$data['data--entities']['user']['relation_role_ws_user']->name = 'relation_role_ws_user';
+  cache::$data['data--entities']['user']['relation_role_ws_user']->storage_id = 'main';
+  cache::$data['data--entities']['user']['relation_role_ws_permission'] = new \effectivecore\different_cache();
+  cache::$data['data--entities']['user']['relation_role_ws_permission']->cache_name = 'data--entities-user-relation_role_ws_permission';
+  cache::$data['data--entities']['user']['relation_role_ws_permission']->name = 'relation_role_ws_permission';
+  cache::$data['data--entities']['user']['relation_role_ws_permission']->storage_id = 'main';
+
+}
