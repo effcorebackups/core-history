6ec734901e7daf0d111b8cc78c428130c1f3c70c
Maxim Rysevets <root@effcore.com>
2017-12-30 18:10:17 +0300

    Iteration #311-426-49

--- dynamic/cache/cache--classes_map.php
+++ dynamic/cache/cache--classes_map.php
@@ -2,7 +2,7 @@
 
 namespace effectivecore { # cache for classes_map
 
-  cache::$info['classes_map']['build'] = '2017-12-30 15:06:03';
+  cache::$info['classes_map']['build'] = '2017-12-30 15:09:43';
   cache::$data['classes_map']['effectivecore\file'] = new \stdClass();
   cache::$data['classes_map']['effectivecore\file']->type = 'class';
   cache::$data['classes_map']['effectivecore\file']->namespace = 'effectivecore';
