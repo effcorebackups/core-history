da8bfa629fd757331af7dd4bef68d57d1a95f0b0
Maxim Rysevets <root@effcore.com>
2017-12-30 22:02:21 +0300

    Iteration #311-426-72

---
+++ dynamic/cache/cache--data--captcha_characters.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace effectivecore { # cache for data--captcha_characters
+
+  cache::$data['data--captcha_characters']['page']['ch0'] = new \stdClass();
+  cache::$data['data--captcha_characters']['page']['ch0']->character = 0;
+  cache::$data['data--captcha_characters']['page']['ch0']->glyphs['default'] = '01110|10001|10001|10001|10001|10001|10001|10001|10001|01110';
+  cache::$data['data--captcha_characters']['page']['ch1'] = new \stdClass();
+  cache::$data['data--captcha_characters']['page']['ch1']->character = 1;
+  cache::$data['data--captcha_characters']['page']['ch1']->glyphs['default'] = '00001|00001|00001|00001|00001|10001|01001|00101|00010|00001';
+  cache::$data['data--captcha_characters']['page']['ch2'] = new \stdClass();
+  cache::$data['data--captcha_characters']['page']['ch2']->character = 2;
+  cache::$data['data--captcha_characters']['page']['ch2']->glyphs['default'] = '11111|10000|01000|00100|00010|00001|00001|00001|00001|11110';
+  cache::$data['data--captcha_characters']['page']['ch3'] = new \stdClass();
+  cache::$data['data--captcha_characters']['page']['ch3']->character = 3;
+  cache::$data['data--captcha_characters']['page']['ch3']->glyphs['default'] = '01000|00100|00010|00001|11111|01000|00100|00010|00001|11111';
+  cache::$data['data--captcha_characters']['page']['ch4'] = new \stdClass();
+  cache::$data['data--captcha_characters']['page']['ch4']->character = 4;
+  cache::$data['data--captcha_characters']['page']['ch4']->glyphs['default'] = '00001|00001|00001|00001|01111|10001|01001|00101|00010|00001';
+  cache::$data['data--captcha_characters']['page']['ch5'] = new \stdClass();
+  cache::$data['data--captcha_characters']['page']['ch5']->character = 5;
+  cache::$data['data--captcha_characters']['page']['ch5']->glyphs['default'] = '01000|00100|00010|00001|01111|10000|10000|10000|10000|01111';
+  cache::$data['data--captcha_characters']['page']['ch6'] = new \stdClass();
+  cache::$data['data--captcha_characters']['page']['ch6']->character = 6;
+  cache::$data['data--captcha_characters']['page']['ch6']->glyphs['default'] = '01110|10001|10001|10001|10001|01110|10000|01000|00100|00010';
+  cache::$data['data--captcha_characters']['page']['ch7'] = new \stdClass();
+  cache::$data['data--captcha_characters']['page']['ch7']->character = 7;
+  cache::$data['data--captcha_characters']['page']['ch7']->glyphs['default'] = '10000|10000|10000|10000|10000|01000|00100|00010|00001|11111';
+  cache::$data['data--captcha_characters']['page']['ch8'] = new \stdClass();
+  cache::$data['data--captcha_characters']['page']['ch8']->character = 8;
+  cache::$data['data--captcha_characters']['page']['ch8']->glyphs['default'] = '01110|10001|10001|10001|10001|01110|10001|10001|10001|01110';
+  cache::$data['data--captcha_characters']['page']['ch9'] = new \stdClass();
+  cache::$data['data--captcha_characters']['page']['ch9']->character = 9;
+  cache::$data['data--captcha_characters']['page']['ch9']->glyphs['default'] = '01000|00100|00010|00001|01110|10001|10001|10001|10001|01110';
+
+}
