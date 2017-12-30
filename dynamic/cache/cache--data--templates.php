7c6ab10a2c75d2167811bf6c9ca0b77542a5b798
Maxim Rysevets <root@effcore.com>
2017-12-30 22:12:55 +0300

    Iteration #311-426-75

---
+++ dynamic/cache/cache--data--templates.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace effectivecore { # cache for data--templates
+
+  cache::$data['data--templates']['page']['markup_html'] = new \stdClass();
+  cache::$data['data--templates']['page']['markup_html']->type = 'inline';
+  cache::$data['data--templates']['page']['markup_html']->markup = '<%%_tag_name %%_attributes>%%_content</%%_tag_name>';
+  cache::$data['data--templates']['page']['markup_html_simple'] = new \stdClass();
+  cache::$data['data--templates']['page']['markup_html_simple']->type = 'inline';
+  cache::$data['data--templates']['page']['markup_html_simple']->markup = '<%%_tag_name %%_attributes>';
+  cache::$data['data--templates']['page']['markup_xml'] = new \stdClass();
+  cache::$data['data--templates']['page']['markup_xml']->type = 'inline';
+  cache::$data['data--templates']['page']['markup_xml']->markup = '<%%_tag_name %%_attributes>%%_content</%%_tag_name>';
+  cache::$data['data--templates']['page']['markup_xml_simple'] = new \stdClass();
+  cache::$data['data--templates']['page']['markup_xml_simple']->type = 'inline';
+  cache::$data['data--templates']['page']['markup_xml_simple']->markup = '<%%_tag_name %%_attributes />';
+  cache::$data['data--templates']['page']['form_container'] = new \stdClass();
+  cache::$data['data--templates']['page']['form_container']->type = 'inline';
+  cache::$data['data--templates']['page']['form_container']->markup = '<%%_tag_name %%_attributes> %%_title_t %%_content %%_title_b %%_description </%%_tag_name>';
+  cache::$data['data--templates']['page']['page'] = new \stdClass();
+  cache::$data['data--templates']['page']['page']->type = 'file';
+  cache::$data['data--templates']['page']['page']->path = 'frontend/template--page.html';
+  cache::$data['data--templates']['page']['page_access_denided'] = new \stdClass();
+  cache::$data['data--templates']['page']['page_access_denided']->type = 'file';
+  cache::$data['data--templates']['page']['page_access_denided']->path = 'frontend/template--page--access_denided.html';
+  cache::$data['data--templates']['page']['page_not_found'] = new \stdClass();
+  cache::$data['data--templates']['page']['page_not_found']->type = 'file';
+  cache::$data['data--templates']['page']['page_not_found']->path = 'frontend/template--page--not_found.html';
+  cache::$data['data--templates']['page']['page_simple'] = new \stdClass();
+  cache::$data['data--templates']['page']['page_simple']->type = 'file';
+  cache::$data['data--templates']['page']['page_simple']->path = 'frontend/template--page_simple.html';
+  cache::$data['data--templates']['page']['canvas_svg'] = new \stdClass();
+  cache::$data['data--templates']['page']['canvas_svg']->type = 'file';
+  cache::$data['data--templates']['page']['canvas_svg']->path = 'frontend/template--canvas_svg.xml';
+  cache::$data['data--templates']['tree']['tree'] = new \stdClass();
+  cache::$data['data--templates']['tree']['tree']->type = 'inline';
+  cache::$data['data--templates']['tree']['tree']->markup = '<x-tree %%_attributes> <h3 class="hidden">%%_self</h3> <ul>%%_children</ul> </x-tree>';
+  cache::$data['data--templates']['tree']['tree_item'] = new \stdClass();
+  cache::$data['data--templates']['tree']['tree_item']->type = 'inline';
+  cache::$data['data--templates']['tree']['tree_item']->markup = '<li %%_attributes> %%_self %%_children </li>';
+  cache::$data['data--templates']['tree']['tree_item_children'] = new \stdClass();
+  cache::$data['data--templates']['tree']['tree_item_children']->type = 'inline';
+  cache::$data['data--templates']['tree']['tree_item_children']->markup = '<ul> %%_children </ul>';
+
+}
