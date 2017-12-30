06ba81e752374f68396bc37890c5215e48f327fe
Maxim Rysevets <root@effcore.com>
2017-12-30 17:58:26 +0300

    Iteration #311-426-46

---
+++ dynamic/cache/cache--data--file_type.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace effectivecore { # cache for data--file_type
+
+  cache::$data['data--file_type']['core']['php'] = new \stdClass();
+  cache::$data['data--file_type']['core']['php']->protected = 1;
+  cache::$data['data--file_type']['core']['php']->headers['Content-type'] = 'text/html';
+  cache::$data['data--file_type']['core']['css'] = new \stdClass();
+  cache::$data['data--file_type']['core']['css']->use_tokens = 1;
+  cache::$data['data--file_type']['core']['css']->headers['Content-type'] = 'text/css';
+  cache::$data['data--file_type']['core']['js'] = new \stdClass();
+  cache::$data['data--file_type']['core']['js']->use_tokens = 1;
+  cache::$data['data--file_type']['core']['js']->headers['Content-type'] = 'text/javascript';
+  cache::$data['data--file_type']['core']['html'] = new \stdClass();
+  cache::$data['data--file_type']['core']['html']->use_tokens = 1;
+  cache::$data['data--file_type']['core']['html']->headers['Content-type'] = 'text/html';
+  cache::$data['data--file_type']['core']['xml'] = new \stdClass();
+  cache::$data['data--file_type']['core']['xml']->use_tokens = 1;
+  cache::$data['data--file_type']['core']['xml']->headers['Content-type'] = 'text/xml';
+  cache::$data['data--file_type']['core']['txt'] = new \stdClass();
+  cache::$data['data--file_type']['core']['txt']->headers['Content-type'] = 'text/plain';
+  cache::$data['data--file_type']['core']['image'] = new \stdClass();
+  cache::$data['data--file_type']['core']['image']->headers['Content-type'] = 'image/*';
+  cache::$data['data--file_type']['core']['svg'] = new \stdClass();
+  cache::$data['data--file_type']['core']['svg']->use_tokens = 1;
+  cache::$data['data--file_type']['core']['svg']->headers['Content-type'] = 'image/svg+xml';
+  cache::$data['data--file_type']['core']['ico'] = new \stdClass();
+  cache::$data['data--file_type']['core']['ico']->headers['Content-type'] = 'image/x-icon';
+  cache::$data['data--file_type']['core']['gif'] = new \stdClass();
+  cache::$data['data--file_type']['core']['gif']->headers['Content-type'] = 'image/gif';
+  cache::$data['data--file_type']['core']['jpg'] = new \stdClass();
+  cache::$data['data--file_type']['core']['jpg']->headers['Content-type'] = 'image/jpeg';
+  cache::$data['data--file_type']['core']['png'] = new \stdClass();
+  cache::$data['data--file_type']['core']['png']->headers['Content-type'] = 'image/png';
+  cache::$data['data--file_type']['storage']['_d'] = new \stdClass();
+  cache::$data['data--file_type']['storage']['_d']->protected = 1;
+  cache::$data['data--file_type']['storage']['_d']->headers['Content-type'] = 'text/data';
+  cache::$data['data--file_type']['storage']['sqlite'] = new \stdClass();
+  cache::$data['data--file_type']['storage']['sqlite']->protected = 1;
+
+}
