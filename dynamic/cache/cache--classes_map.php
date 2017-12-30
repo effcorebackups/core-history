53d6d07a4bdbbe1406cfdd697f862eaffb0db2e4
Maxim Rysevets <root@effcore.com>
2017-12-30 18:13:05 +0300

    Iteration #311-426-52

--- dynamic/cache/cache--classes_map.php
+++ dynamic/cache/cache--classes_map.php
@@ -2,7 +2,7 @@
 
 namespace effectivecore { # cache for classes_map
 
-  cache::$info['classes_map']['build'] = '2017-12-30 15:11:54';
+  cache::$info['classes_map']['build'] = '2017-12-30 15:12:51';
   cache::$data['classes_map']['effectivecore\file'] = new \stdClass();
   cache::$data['classes_map']['effectivecore\file']->type = 'class';
   cache::$data['classes_map']['effectivecore\file']->namespace = 'effectivecore';
--- dynamic/cache/cache--data_original.php
+++ dynamic/cache/cache--data_original.php
@@ -2,7 +2,7 @@
 
 namespace effectivecore { # cache for data_original
 
-  cache::$info['data_original']['build'] = '2017-12-30 15:11:54';
+  cache::$info['data_original']['build'] = '2017-12-30 15:12:51';
   cache::$data['data_original']['event']['core'] = new \stdClass();
   cache::$data['data_original']['event']['core']->on_form_init['installation'] = new \effectivecore\event();
   cache::$data['data_original']['event']['core']->on_form_init['installation']->for = 'installation';
