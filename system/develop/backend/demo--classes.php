80d6ae90d16f938d918c8fad90c3337aae6d7390
Maxim Rysevets <root@effcore.com>
2018-03-02 14:18:30 +0300

    Iteration #311-426-159

--- system/develop/backend/demo--classes.php
+++ system/develop/backend/demo--classes.php
@@ -6,22 +6,14 @@
 
 namespace effcore\modules\develop {
           use const \effcore\br;
-          use const \effcore\nl;
-          use const \effcore\tb;
           use \effcore\markup as markup;
-          use \effcore\locale as locale;
-          use \effcore\module as module;
           abstract class demo_class_1 extends \effcore\factory {
 
 }}
 
 namespace effcore\modules\develop {
-          use const \effcore\br;
           use const \effcore\nl;
-          use const \effcore\tb;
-          use \effcore\markup as markup;
           use \effcore\locale as locale;
-          use \effcore\module as module;
           abstract class demo_class_2 extends \effcore\factory {
 
 }}
