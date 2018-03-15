3f77a999728a2496d8c2b4f4c52913355c1878a1
Maxim Rysevets <root@effcore.com>
2018-03-15 14:51:23 +0300

    Iteration #312-14

--- dynamic/cache/cache--data_original.php
+++ dynamic/cache/cache--data_original.php
@@ -2,7 +2,7 @@
 
 namespace effcore { # cache for data_original
 
-  cache::$info['data_original']['build'] = '2018-03-15 11:43:03';
+  cache::$info['data_original']['build'] = '2018-03-15 11:49:59';
   cache::$data['data_original']['bundle']['global'] = new \stdClass();
   cache::$data['data_original']['bundle']['global']->id = 'system';
   cache::$data['data_original']['bundle']['global']->title = 'System';
