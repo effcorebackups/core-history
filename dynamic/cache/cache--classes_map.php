f3240f6e0e07872501a0dfbd5d68962c0f0a15ea
Maxim Rysevets <root@effcore.com>
2018-03-15 11:17:27 +0300

    Iteration #312-4

--- dynamic/cache/cache--classes_map.php
+++ dynamic/cache/cache--classes_map.php
@@ -2,7 +2,7 @@
 
 namespace effcore { # cache for classes_map
 
-  cache::$info['classes_map']['build'] = '2018-03-14 21:20:19';
+  cache::$info['classes_map']['build'] = '2018-03-15 08:15:56';
   cache::$data['classes_map']['effcore\file'] = new \stdClass();
   cache::$data['classes_map']['effcore\file']->type = 'class';
   cache::$data['classes_map']['effcore\file']->namespace = 'effcore';
--- dynamic/cache/cache--data_original.php
+++ dynamic/cache/cache--data_original.php
@@ -2,7 +2,7 @@
 
 namespace effcore { # cache for data_original
 
-  cache::$info['data_original']['build'] = '2018-03-14 21:20:19';
+  cache::$info['data_original']['build'] = '2018-03-15 08:15:56';
   cache::$data['data_original']['bundle']['global'] = new \stdClass();
   cache::$data['data_original']['bundle']['global']->id = 'system';
   cache::$data['data_original']['bundle']['global']->title = 'System';
@@ -1791,38 +1791,6 @@ namespace effcore { # cache for data_original
   cache::$data['data_original']['tree_items']['user']['users']->attributes['class']['users'] = 'users';
   cache::$data['data_original']['tree_items']['user']['users']->access = new \stdClass();
   cache::$data['data_original']['tree_items']['user']['users']->access->roles['admins'] = 'admins';
-  cache::$data['data_original']['tree_items']['user']['users_select'] = new \effcore\tab_item();
-  cache::$data['data_original']['tree_items']['user']['users_select']->id = 'users_select';
-  cache::$data['data_original']['tree_items']['user']['users_select']->id_parent = 'users';
-  cache::$data['data_original']['tree_items']['user']['users_select']->title = 'Select';
-  cache::$data['data_original']['tree_items']['user']['users_select']->attributes['href'] = '/admin/users/select';
-  cache::$data['data_original']['tree_items']['user']['users_select']->attributes['class']['users-select'] = 'users-select';
-  cache::$data['data_original']['tree_items']['user']['users_select']->access = new \stdClass();
-  cache::$data['data_original']['tree_items']['user']['users_select']->access->roles['admins'] = 'admins';
-  cache::$data['data_original']['tree_items']['user']['users_insert'] = new \effcore\tab_item();
-  cache::$data['data_original']['tree_items']['user']['users_insert']->id = 'users_insert';
-  cache::$data['data_original']['tree_items']['user']['users_insert']->id_parent = 'users';
-  cache::$data['data_original']['tree_items']['user']['users_insert']->title = 'Insert';
-  cache::$data['data_original']['tree_items']['user']['users_insert']->attributes['href'] = '/admin/users/insert';
-  cache::$data['data_original']['tree_items']['user']['users_insert']->attributes['class']['users-insert'] = 'users-insert';
-  cache::$data['data_original']['tree_items']['user']['users_insert']->access = new \stdClass();
-  cache::$data['data_original']['tree_items']['user']['users_insert']->access->roles['admins'] = 'admins';
-  cache::$data['data_original']['tree_items']['user']['users_update'] = new \effcore\tab_item();
-  cache::$data['data_original']['tree_items']['user']['users_update']->id = 'users_update';
-  cache::$data['data_original']['tree_items']['user']['users_update']->id_parent = 'users';
-  cache::$data['data_original']['tree_items']['user']['users_update']->title = 'Update';
-  cache::$data['data_original']['tree_items']['user']['users_update']->attributes['href'] = '/admin/users/update';
-  cache::$data['data_original']['tree_items']['user']['users_update']->attributes['class']['users-update'] = 'users-update';
-  cache::$data['data_original']['tree_items']['user']['users_update']->access = new \stdClass();
-  cache::$data['data_original']['tree_items']['user']['users_update']->access->roles['admins'] = 'admins';
-  cache::$data['data_original']['tree_items']['user']['users_delete'] = new \effcore\tab_item();
-  cache::$data['data_original']['tree_items']['user']['users_delete']->id = 'users_delete';
-  cache::$data['data_original']['tree_items']['user']['users_delete']->id_parent = 'users';
-  cache::$data['data_original']['tree_items']['user']['users_delete']->title = 'Delete';
-  cache::$data['data_original']['tree_items']['user']['users_delete']->attributes['href'] = '/admin/users/delete';
-  cache::$data['data_original']['tree_items']['user']['users_delete']->attributes['class']['users-delete'] = 'users-delete';
-  cache::$data['data_original']['tree_items']['user']['users_delete']->access = new \stdClass();
-  cache::$data['data_original']['tree_items']['user']['users_delete']->access->roles['admins'] = 'admins';
   cache::$data['data_original']['trees']['core']['main'] = new \effcore\tree();
   cache::$data['data_original']['trees']['core']['main']->id = 'main';
   cache::$data['data_original']['trees']['core']['main']->title = 'Main menu';
@@ -2935,5 +2903,37 @@ namespace effcore { # cache for data_original
   cache::$data['data_original']['storages']['storage']['storage_pdo_sql']->id = 'main';
   cache::$data['data_original']['storages']['storage']['storage_files_nosql'] = new \effcore\storage_files();
   cache::$data['data_original']['storages']['storage']['storage_files_nosql']->id = 'files';
+  cache::$data['data_original']['tab_items']['user']['users_select'] = new \effcore\tab_item();
+  cache::$data['data_original']['tab_items']['user']['users_select']->id = 'users_select';
+  cache::$data['data_original']['tab_items']['user']['users_select']->id_parent = 'users';
+  cache::$data['data_original']['tab_items']['user']['users_select']->title = 'Select';
+  cache::$data['data_original']['tab_items']['user']['users_select']->attributes['href'] = '/admin/users/select';
+  cache::$data['data_original']['tab_items']['user']['users_select']->attributes['class']['users-select'] = 'users-select';
+  cache::$data['data_original']['tab_items']['user']['users_select']->access = new \stdClass();
+  cache::$data['data_original']['tab_items']['user']['users_select']->access->roles['admins'] = 'admins';
+  cache::$data['data_original']['tab_items']['user']['users_insert'] = new \effcore\tab_item();
+  cache::$data['data_original']['tab_items']['user']['users_insert']->id = 'users_insert';
+  cache::$data['data_original']['tab_items']['user']['users_insert']->id_parent = 'users';
+  cache::$data['data_original']['tab_items']['user']['users_insert']->title = 'Insert';
+  cache::$data['data_original']['tab_items']['user']['users_insert']->attributes['href'] = '/admin/users/insert';
+  cache::$data['data_original']['tab_items']['user']['users_insert']->attributes['class']['users-insert'] = 'users-insert';
+  cache::$data['data_original']['tab_items']['user']['users_insert']->access = new \stdClass();
+  cache::$data['data_original']['tab_items']['user']['users_insert']->access->roles['admins'] = 'admins';
+  cache::$data['data_original']['tab_items']['user']['users_update'] = new \effcore\tab_item();
+  cache::$data['data_original']['tab_items']['user']['users_update']->id = 'users_update';
+  cache::$data['data_original']['tab_items']['user']['users_update']->id_parent = 'users';
+  cache::$data['data_original']['tab_items']['user']['users_update']->title = 'Update';
+  cache::$data['data_original']['tab_items']['user']['users_update']->attributes['href'] = '/admin/users/update';
+  cache::$data['data_original']['tab_items']['user']['users_update']->attributes['class']['users-update'] = 'users-update';
+  cache::$data['data_original']['tab_items']['user']['users_update']->access = new \stdClass();
+  cache::$data['data_original']['tab_items']['user']['users_update']->access->roles['admins'] = 'admins';
+  cache::$data['data_original']['tab_items']['user']['users_delete'] = new \effcore\tab_item();
+  cache::$data['data_original']['tab_items']['user']['users_delete']->id = 'users_delete';
+  cache::$data['data_original']['tab_items']['user']['users_delete']->id_parent = 'users';
+  cache::$data['data_original']['tab_items']['user']['users_delete']->title = 'Delete';
+  cache::$data['data_original']['tab_items']['user']['users_delete']->attributes['href'] = '/admin/users/delete';
+  cache::$data['data_original']['tab_items']['user']['users_delete']->attributes['class']['users-delete'] = 'users-delete';
+  cache::$data['data_original']['tab_items']['user']['users_delete']->access = new \stdClass();
+  cache::$data['data_original']['tab_items']['user']['users_delete']->access->roles['admins'] = 'admins';
 
 }
