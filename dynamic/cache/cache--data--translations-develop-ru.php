3d73daf12a63528e6fa4fb9688412a72c049c6ca
Maxim Rysevets <root@effcore.com>
2018-03-20 14:21:11 +0300

    Iteration #312-53

---
+++ dynamic/cache/cache--data--translations-develop-ru.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace effcore { # cache for data--translations-develop-ru
+
+  cache::$data['data--translations-develop-ru'] = new \effcore\translation();
+  cache::$data['data--translations-develop-ru']->code = 'ru';
+  cache::$data['data--translations-develop-ru']->data['Call %%_name'] = 'Вызов %%_name';
+  cache::$data['data--translations-develop-ru']->data['call'] = 'вызов';
+  cache::$data['data--translations-develop-ru']->data['cell %%_num'] = 'ячейка %%_num';
+  cache::$data['data--translations-develop-ru']->data['Checkboxes (macro)'] = 'Флажки (макро)';
+  cache::$data['data--translations-develop-ru']->data['Checkboxes'] = 'Флажки';
+  cache::$data['data--translations-develop-ru']->data['class'] = 'класс';
+  cache::$data['data--translations-develop-ru']->data['Classes list'] = 'Список классов';
+  cache::$data['data--translations-develop-ru']->data['Classes'] = 'Классы';
+  cache::$data['data--translations-develop-ru']->data['Console'] = 'Консоль';
+  cache::$data['data--translations-develop-ru']->data['Demo'] = 'Демо';
+  cache::$data['data--translations-develop-ru']->data['Develop'] = 'Разработка';
+  cache::$data['data--translations-develop-ru']->data['Dynamic block'] = 'Динамический блок';
+  cache::$data['data--translations-develop-ru']->data['Element %%_info.'] = 'Элемент %%_info.';
+  cache::$data['data--translations-develop-ru']->data['Elements'] = 'Элементы';
+  cache::$data['data--translations-develop-ru']->data['Error message #%%_num.'] = 'Сообщение об ошибке #%%_num.';
+  cache::$data['data--translations-develop-ru']->data['event system was initialized'] = 'система событий была инициализирована';
+  cache::$data['data--translations-develop-ru']->data['Execute plan'] = 'План выполнения';
+  cache::$data['data--translations-develop-ru']->data['head cell %%_num'] = 'заглавная ячейка %%_num';
+  cache::$data['data--translations-develop-ru']->data['init.'] = 'инициал.';
+  cache::$data['data--translations-develop-ru']->data['interface'] = 'интерфейс';
+  cache::$data['data--translations-develop-ru']->data['intr.'] = 'интр.';
+  cache::$data['data--translations-develop-ru']->data['Module for demonstrating capabilities.'] = 'Модуль для демонстрации возможностей.';
+  cache::$data['data--translations-develop-ru']->data['Module for development.'] = 'Модуль для разработки.';
+  cache::$data['data--translations-develop-ru']->data['Module for testing.'] = 'Модуль для тестирования.';
+  cache::$data['data--translations-develop-ru']->data['Notice message #%%_num.'] = 'Уведомление #%%_num.';
+  cache::$data['data--translations-develop-ru']->data['Ok message #%%_num.'] = 'Хорошее сообщение #%%_num.';
+  cache::$data['data--translations-develop-ru']->data['Ordered list'] = 'Упорядоченный список';
+  cache::$data['data--translations-develop-ru']->data['query = "%%_query"'] = 'запрос = "%%_query"';
+  cache::$data['data--translations-develop-ru']->data['query = "%%_query"<br/>args = [%%_args]'] = 'запрос = "%%_query"<br/>аргументы = [%%_args]';
+  cache::$data['data--translations-develop-ru']->data['Radios (macro)'] = 'Переключатели (макро)';
+  cache::$data['data--translations-develop-ru']->data['Radios'] = 'Переключатели';
+  cache::$data['data--translations-develop-ru']->data['Selection list (macro)'] = 'Список выбора (макро)';
+  cache::$data['data--translations-develop-ru']->data['Selection list'] = 'Список выбора';
+  cache::$data['data--translations-develop-ru']->data['Static block'] = 'Статический блок';
+  cache::$data['data--translations-develop-ru']->data['Textarea'] = 'Текстовая область';
+  cache::$data['data--translations-develop-ru']->data['Total build time'] = 'Общее время сборки';
+  cache::$data['data--translations-develop-ru']->data['Total load'] = 'Общая загрука';
+  cache::$data['data--translations-develop-ru']->data['UNDER CONSTRUCTION'] = 'В РАЗРАБОТКЕ';
+  cache::$data['data--translations-develop-ru']->data['Unordered list'] = 'Неупорядоченный список';
+  cache::$data['data--translations-develop-ru']->data['Warning message #%%_num.'] = 'Предупреждающее сообщение #%%_num.';
+
+}
