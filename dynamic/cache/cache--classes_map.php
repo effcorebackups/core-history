ee19a3b95a26c18060769b93e41d0af62a21831b
Maxim Rysevets <root@effcore.com>
2018-03-24 17:28:55 +0300

    Iteration #312-78

--- dynamic/cache/cache--classes_map.php
+++ dynamic/cache/cache--classes_map.php
@@ -2,7 +2,7 @@
 
 namespace effcore { # cache for classes_map
 
-  cache::$info['classes_map']['build'] = '2018-03-24 12:42:55';
+  cache::$info['classes_map']['build'] = '2018-03-24 14:28:35';
   cache::$data['classes_map']['effcore\file'] = new \stdClass();
   cache::$data['classes_map']['effcore\file']->type = 'class';
   cache::$data['classes_map']['effcore\file']->namespace = 'effcore';
