4de21dcde25d657239bde03f2124d73369eee75d
Maxim Rysevets <root@effcore.com>
2018-03-25 11:36:54 +0300

    Iteration #312-85

--- system/develop/demo/backend/class--demo_classes.php
+++ system/develop/demo/backend/class--demo_classes.php
@@ -5,15 +5,50 @@
   ##################################################################
 
 namespace effcore\modules\demo {
+          const my_const_2 = 300;
           use const \effcore\br;
           use \effcore\markup;
           class demo_class_1 {
 
+  public    $property_1;
+  protected $property_2;
+  private   $property_3 = 'default string';
+
+  public function method_1() {return $this->property_3;} # return 'default string'
+
+  ###########################
+  ### static declarations ###
+  ###########################
+
+  static public    $static_property_1;
+  static protected $static_property_2;
+  static private   $static_property_3 = my_const_2;
+
+  static public function static_method_1() {return static::$static_property_3;} # return 300
+
 }}
+
+
 
 namespace effcore\modules\demo {
           use const \effcore\nl;
           use \effcore\locale;
           class demo_class_2 extends demo_class_1 {
+
+  private $property_3 = 'modified string';
+  private $property_4 = 'new property #4';
+
+  public function method_1() {return $this->property_3;} # return 'modified string'
+  public function method_2() {return $this->property_4;} # return 'new property #4'
+
+  ###########################
+  ### static declarations ###
+  ###########################
+
+  static private $static_property_3 = my_const_2 + 100;
+  static private $static_property_4 = 'new static property #4';
+
+  static public function static_method_1() {return static::$static_property_3;} # return 400
+  static public function static_method_2() {return static::$static_property_4;} # return 'new static property #4'
 
 }}
--- system/develop/demo/backend/class_ab--demo_classes.php
+++ system/develop/demo/backend/class_ab--demo_classes.php
@@ -5,15 +5,30 @@
   ##################################################################
 
 namespace effcore\modules\demo {
+          const my_const_1 = 300;
           use const \effcore\br;
           use \effcore\markup;
           abstract class demo_class_ab_1 {
 
+  static public    $property_1;
+  static protected $property_2;
+  static private   $property_3 = my_const_1;
+
+  static public function static_method_1() {return static::$property_3;} # return 300
+
 }}
+
+
 
 namespace effcore\modules\demo {
           use const \effcore\nl;
           use \effcore\locale;
           abstract class demo_class_ab_2 extends demo_class_ab_1 {
+
+  static private $property_3 = my_const_1 + 100;
+  static private $property_4 = 'new property #4';
+
+  static public function static_method_1() {return static::$property_3;} # return 400
+  static public function static_method_2() {return static::$property_4;} # return 'new property #4'
 
 }}
