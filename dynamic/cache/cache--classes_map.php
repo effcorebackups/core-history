ad1448e42b7e056c9ab2ebb5ee7550c34e08b04e
Maxim Rysevets <root@effcore.com>
2018-04-20 11:39:21 +0300

    Iteration #312-183

--- dynamic/cache/cache--classes_map.php
+++ dynamic/cache/cache--classes_map.php
@@ -2,7 +2,7 @@
 
 namespace effcore { # cache for classes_map
 
-  cache::$info['classes_map']['build'] = '2018-04-20 07:49:51';
+  cache::$info['classes_map']['build'] = '2018-04-20 08:38:44';
   cache::$data['classes_map']['effcore\file'] = new \stdClass();
   cache::$data['classes_map']['effcore\file']->namespace = 'effcore';
   cache::$data['classes_map']['effcore\file']->name = 'file';
--- dynamic/cache/cache--data_original.php
+++ dynamic/cache/cache--data_original.php
@@ -2,7 +2,7 @@
 
 namespace effcore { # cache for data_original
 
-  cache::$info['data_original']['build'] = '2018-04-20 07:49:51';
+  cache::$info['data_original']['build'] = '2018-04-20 08:38:44';
   cache::$data['data_original']['trees']['user']['user_anonymous'] = new \effcore\tree();
   cache::$data['data_original']['trees']['user']['user_anonymous']->id = 'user_anonymous';
   cache::$data['data_original']['trees']['user']['user_anonymous']->title = 'User anonymous menu';
