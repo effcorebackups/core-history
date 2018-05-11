2b077f17c0be343a0cc062c480a9780f46c597e1
Maxim Rysevets <root@effcore.com>
2018-05-11 12:05:58 +0300

    Iteration #312-246-50

--- dynamic/cache/cache--data_original.php
+++ dynamic/cache/cache--data_original.php
@@ -2,7 +2,7 @@
 
 namespace effcore { # cache for data_original
 
-  cache::$info['data_original']['build'] = '2018-05-11 08:58:56';
+  cache::$info['data_original']['build'] = '2018-05-11 09:05:36';
   cache::$data['data_original']['trees']['user']['user_anonymous'] = new \effcore\tree();
   cache::$data['data_original']['trees']['user']['user_anonymous']->id = 'user_anonymous';
   cache::$data['data_original']['trees']['user']['user_anonymous']->title = 'User anonymous menu';
--- dynamic/cache/cache--structures.php
+++ dynamic/cache/cache--structures.php
@@ -2,7 +2,7 @@
 
 namespace effcore { # cache for structures
 
-  cache::$info['structures']['build'] = '2018-05-11 08:58:56';
+  cache::$info['structures']['build'] = '2018-05-11 09:05:35';
   cache::$data['structures']['effcore\\access'] = new \stdClass();
   cache::$data['structures']['effcore\\access']->modifier = 'abstract';
   cache::$data['structures']['effcore\\access']->namespace = 'effcore';
