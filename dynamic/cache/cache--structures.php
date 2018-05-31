8ff642ea1daa8b38565c61f2efd860a91e748e10
Maxim Rysevets <root@effcore.com>
2018-05-31 22:11:00 +0300

    Iteration #312-246-129

--- dynamic/cache/cache--structures.php
+++ dynamic/cache/cache--structures.php
@@ -2,7 +2,7 @@
 
 namespace effcore { # cache for structures
 
-  cache::$info['structures']['build'] = '2018-05-31 18:07:27';
+  cache::$info['structures']['build'] = '2018-05-31 19:06:57';
   cache::$data['structures']['effcore\\access'] = new \stdClass();
   cache::$data['structures']['effcore\\access']->modifier = 'abstract';
   cache::$data['structures']['effcore\\access']->namespace = 'effcore';
