d409b98a0402b1d3f14b43f86552520472092672
Maxim Rysevets <root@effcore.com>
2018-07-27 18:20:56 +0300

    Iteration #312-246-300

--- dynamic/cache/cache--data--tests-test-load.php
+++ dynamic/cache/cache--data--tests-test-load.php
@@ -10,7 +10,7 @@ namespace effcore { # cache for data--tests-test-load
   cache::$data['data--tests-test-load']->scenario['request_page_login']->https = false;
   cache::$data['data--tests-test-load']->scenario['response_check'] = new \effcore\step();
   cache::$data['data--tests-test-load']->scenario['response_check']->type = 'check';
-  cache::$data['data--tests-test-load']->scenario['response_check']->where = 'response';
+  cache::$data['data--tests-test-load']->scenario['response_check']->where = 'http_code';
   cache::$data['data--tests-test-load']->scenario['response_check']->match = 200;
   cache::$data['data--tests-test-load']->scenario['response_check']->on_success['request_page_registration'] = new \effcore\step();
   cache::$data['data--tests-test-load']->scenario['response_check']->on_success['request_page_registration']->type = 'request';
@@ -18,7 +18,7 @@ namespace effcore { # cache for data--tests-test-load
   cache::$data['data--tests-test-load']->scenario['response_check']->on_success['request_page_registration']->https = false;
   cache::$data['data--tests-test-load']->scenario['response_check']->on_success['response_check'] = new \effcore\step();
   cache::$data['data--tests-test-load']->scenario['response_check']->on_success['response_check']->type = 'check';
-  cache::$data['data--tests-test-load']->scenario['response_check']->on_success['response_check']->where = 'response';
+  cache::$data['data--tests-test-load']->scenario['response_check']->on_success['response_check']->where = 'http_code';
   cache::$data['data--tests-test-load']->scenario['response_check']->on_success['response_check']->match = 200;
   cache::$data['data--tests-test-load']->scenario['response_check']->on_success['response_check']->on_success['return_result'] = new \effcore\step();
   cache::$data['data--tests-test-load']->scenario['response_check']->on_success['response_check']->on_success['return_result']->type = 'return';
--- dynamic/cache/cache--structures.php
+++ dynamic/cache/cache--structures.php
@@ -2,7 +2,7 @@
 
 namespace effcore { # cache for structures
 
-  cache::$info['structures']['build'] = '2018-07-27 10:22:36';
+  cache::$info['structures']['build'] = '2018-07-27 15:20:21';
   cache::$data['structures']['effcore\\access'] = new \stdClass();
   cache::$data['structures']['effcore\\access']->modifier = 'abstract';
   cache::$data['structures']['effcore\\access']->namespace = 'effcore';
