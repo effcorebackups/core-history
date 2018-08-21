855986446dc91a143b2c3081d56ff699a29ccff5
Maxim Rysevets <root@effcore.com>
2018-08-21 19:38:00 +0300

    Iteration #312-246-383

--- dynamic/cache/cache--data--entities-demo-demo_autoincrement.php
+++ dynamic/cache/cache--data--entities-demo-demo_autoincrement.php
@@ -7,10 +7,9 @@ namespace effcore { # cache for data--entities-demo-demo_autoincrement
   cache::$data['data--entities-demo-demo_autoincrement']->storage_id = 'main';
   cache::$data['data--entities-demo-demo_autoincrement']->catalog_id = 'demo_autoincrement';
   cache::$data['data--entities-demo-demo_autoincrement']->title = 'Demo: autoincrement';
-  cache::$data['data--entities-demo-demo_autoincrement']->fields = new \stdClass();
-  cache::$data['data--entities-demo-demo_autoincrement']->fields->f_integer_1 = new \stdClass();
-  cache::$data['data--entities-demo-demo_autoincrement']->fields->f_integer_1->type = 'autoincrement';
-  cache::$data['data--entities-demo-demo_autoincrement']->fields->f_integer_1->not_null = true;
+  cache::$data['data--entities-demo-demo_autoincrement']->fields['f_integer_1'] = new \stdClass();
+  cache::$data['data--entities-demo-demo_autoincrement']->fields['f_integer_1']->type = 'autoincrement';
+  cache::$data['data--entities-demo-demo_autoincrement']->fields['f_integer_1']->not_null = true;
   cache::$data['data--entities-demo-demo_autoincrement']->constraints['key_f_integer_1'] = new \stdClass();
   cache::$data['data--entities-demo-demo_autoincrement']->constraints['key_f_integer_1']->type = 'primary key';
   cache::$data['data--entities-demo-demo_autoincrement']->constraints['key_f_integer_1']->fields['f_integer_1'] = 'f_integer_1';
