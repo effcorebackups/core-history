882ea176ceeba477fdc65e541e9c8e8b8258877c
Maxim Rysevets <root@effcore.com>
2018-08-27 23:38:11 +0300

    Iteration #312-246-424

--- dynamic/cache/cache--data_original.php
+++ dynamic/cache/cache--data_original.php
@@ -2,7 +2,7 @@
 
 namespace effcore { # cache for data_original
 
-  cache::$info['data_original']['build'] = '2018-08-27 20:35:27';
+  cache::$info['data_original']['build'] = '2018-08-27 20:37:46';
   cache::$data['data_original']['trees']['user']['user_anonymous'] = new \effcore\tree();
   cache::$data['data_original']['trees']['user']['user_anonymous']->id = 'user_anonymous';
   cache::$data['data_original']['trees']['user']['user_anonymous']->title = 'User anonymous menu';
