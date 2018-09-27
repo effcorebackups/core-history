268cba504e17c50fb122402f0a0520f3946e9ca3
Maxim Rysevets <root@effcore.com>
2018-09-28 00:46:46 +0300

    Iteration #312-327

--- dynamic/cache/cache--data--selections-demo-demo_types_list.php
+++ dynamic/cache/cache--data--selections-demo-demo_types_list.php
@@ -5,25 +5,34 @@ namespace effcore { # cache for data--selections-demo-demo_types_list
   cache::$data['data--selections-demo-demo_types_list'] = new \effcore\selection();
   cache::$data['data--selections-demo-demo_types_list']->title = 'Selection of "demo_types" entities';
   cache::$data['data--selections-demo-demo_types_list']->fields['demo_types.id'] = new \stdClass();
+  cache::$data['data--selections-demo-demo_types_list']->fields['demo_types.id']->type = 'field';
   cache::$data['data--selections-demo-demo_types_list']->fields['demo_types.id']->entity_name = 'demo_types';
   cache::$data['data--selections-demo-demo_types_list']->fields['demo_types.id']->field_name = 'id';
   cache::$data['data--selections-demo-demo_types_list']->fields['demo_types.description'] = new \stdClass();
+  cache::$data['data--selections-demo-demo_types_list']->fields['demo_types.description']->type = 'field';
   cache::$data['data--selections-demo-demo_types_list']->fields['demo_types.description']->entity_name = 'demo_types';
   cache::$data['data--selections-demo-demo_types_list']->fields['demo_types.description']->field_name = 'description';
   cache::$data['data--selections-demo-demo_types_list']->fields['demo_types.f_integer'] = new \stdClass();
+  cache::$data['data--selections-demo-demo_types_list']->fields['demo_types.f_integer']->type = 'field';
   cache::$data['data--selections-demo-demo_types_list']->fields['demo_types.f_integer']->entity_name = 'demo_types';
   cache::$data['data--selections-demo-demo_types_list']->fields['demo_types.f_integer']->field_name = 'f_integer';
   cache::$data['data--selections-demo-demo_types_list']->fields['demo_types.f_varchar'] = new \stdClass();
+  cache::$data['data--selections-demo-demo_types_list']->fields['demo_types.f_varchar']->type = 'field';
   cache::$data['data--selections-demo-demo_types_list']->fields['demo_types.f_varchar']->entity_name = 'demo_types';
   cache::$data['data--selections-demo-demo_types_list']->fields['demo_types.f_varchar']->field_name = 'f_varchar';
   cache::$data['data--selections-demo-demo_types_list']->fields['demo_types.f_time'] = new \stdClass();
+  cache::$data['data--selections-demo-demo_types_list']->fields['demo_types.f_time']->type = 'field';
   cache::$data['data--selections-demo-demo_types_list']->fields['demo_types.f_time']->entity_name = 'demo_types';
   cache::$data['data--selections-demo-demo_types_list']->fields['demo_types.f_time']->field_name = 'f_time';
   cache::$data['data--selections-demo-demo_types_list']->fields['demo_types.f_date'] = new \stdClass();
+  cache::$data['data--selections-demo-demo_types_list']->fields['demo_types.f_date']->type = 'field';
   cache::$data['data--selections-demo-demo_types_list']->fields['demo_types.f_date']->entity_name = 'demo_types';
   cache::$data['data--selections-demo-demo_types_list']->fields['demo_types.f_date']->field_name = 'f_date';
   cache::$data['data--selections-demo-demo_types_list']->fields['demo_types.f_datetime'] = new \stdClass();
+  cache::$data['data--selections-demo-demo_types_list']->fields['demo_types.f_datetime']->type = 'field';
   cache::$data['data--selections-demo-demo_types_list']->fields['demo_types.f_datetime']->entity_name = 'demo_types';
   cache::$data['data--selections-demo-demo_types_list']->fields['demo_types.f_datetime']->field_name = 'f_datetime';
+  cache::$data['data--selections-demo-demo_types_list']->fields['demo_types.actions'] = new \stdClass();
+  cache::$data['data--selections-demo-demo_types_list']->fields['demo_types.actions']->type = 'actions';
 
 }
--- dynamic/cache/cache--structures.php
+++ dynamic/cache/cache--structures.php
@@ -2,7 +2,7 @@
 
 namespace effcore { # cache for structures
 
-  cache::$info['structures']['build'] = '2018-09-27 13:18:20';
+  cache::$info['structures']['build'] = '2018-09-27 21:29:14';
   cache::$data['structures']['effcore\\access'] = new \stdClass();
   cache::$data['structures']['effcore\\access']->modifier = 'abstract';
   cache::$data['structures']['effcore\\access']->namespace = 'effcore';
