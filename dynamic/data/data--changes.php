de7f9c61046832d82bb093d3d23381d9779ea3f8
Maxim Rysevets <root@effcore.com>
2018-10-22 17:04:21 +0300

    Iteration #400-105-19

--- dynamic/data/data--changes.php
+++ dynamic/data/data--changes.php
@@ -2,7 +2,7 @@
 
 namespace effcore { # data for changes
 
-  data::$info['changes']['build'] = '2018-10-22 11:14:01';
+  data::$info['changes']['build'] = '2018-10-22 14:03:52';
   data::$data['changes']['core'] = new \stdClass();
   data::$data['changes']['core']->insert['storages/storage/storage_pdo_sql'] = new \stdClass();
   data::$data['changes']['core']->insert['storages/storage/storage_pdo_sql']->driver = 'mysql';
@@ -18,5 +18,7 @@ namespace effcore { # data for changes
   data::$data['changes']['core']->update['settings/core/keys']['form_validation'] = '9a69052194f26eb04a7169ec64d6ef47363f04f7';
   data::$data['changes']['core']->update['settings/core/keys']['session'] = 'c210859d78f65536912441b14576a9323e7c6586';
   data::$data['changes']['core']->update['settings/core/keys']['salt'] = 'a7b20ad8942a4459c5524d0fcfbdf6dc205844e0';
+  data::$data['changes']['locales'] = new \stdClass();
+  data::$data['changes']['locales']->update['settings/locales/lang_code'] = 'ru';
 
 }
