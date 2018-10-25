dfc9ccf8666e0e6bdf0549aaed4f1a5ed8d30760
Maxim Rysevets <root@effcore.com>
2018-10-25 16:34:45 +0300

    Iteration #400-109

--- dynamic/cache/cache--data--selections.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace effcore { # cache for data--selections
-
-  cache::$data['data--selections']['demo']['demo_types_list'] = new \effcore\external_cache();
-  cache::$data['data--selections']['demo']['demo_types_list']->cache_name = 'data--selections-demo-demo_types_list';
-
-}
--- dynamic/cache/cache--data--storages-storage-storage_files_nosql.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace effcore { # cache for data--storages-storage-storage_files_nosql
-
-  cache::$data['data--storages-storage-storage_files_nosql'] = new \effcore\storage_files();
-  cache::$data['data--storages-storage-storage_files_nosql']->name = 'files';
-
-}
