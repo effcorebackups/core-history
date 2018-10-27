d3f0367af6976e32e50a7d62ab47bafa7abf388a
Maxim Rysevets <root@effcore.com>
2018-10-27 11:18:31 +0300

    Iteration #400-120

--- shell/cache_clear.sh
+++ shell/cache_clear.sh
@@ -1,1 +1,3 @@
+#!/usr/bin/env bash
+
 find ../dynamic/cache/ -not -name 'readme.mark' -delete
--- shell/dynamic_reset.sh
+++ shell/dynamic_reset.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 find ../dynamic/data/ -not -name 'readme.mark' -delete
 cp /dev/null ../dynamic/data/data.sqlite
 ./cache_clear.sh
