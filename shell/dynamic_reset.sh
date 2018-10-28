6f6a10f5a6ee7807c6a04c1f1128dfb083369773
Maxim Rysevets <root@effcore.com>
2018-10-28 11:21:56 +0300

    Iteration #400-129

--- shell/dynamic_reset.sh
+++ shell/dynamic_reset.sh
@@ -3,15 +3,15 @@
 while true; do
   read -p 'All data will be lost. Are you sure?: ' answer
   case $answer in
-      y ) find ../dynamic/cache/   -not -name 'readme.mark' -delete
-          find ../dynamic/data/    -not -name 'readme.mark' -delete
-          find ../dynamic/files/   -not -name 'readme.mark' -delete
-          find ../dynamic/logs/    -not -name 'readme.mark' -delete
-          find ../dynamic/modules/ -not -name 'readme.mark' -delete
-          find ../dynamic/tmp/     -not -name 'readme.mark' -delete
-          cp /dev/null ../dynamic/data/data.sqlite
-          break;;
-      n ) exit;;
-      * ) echo 'Please answer "y" or "n".';;
+    y ) find ../dynamic/cache/   -not -name 'readme.mark' -delete
+        find ../dynamic/data/    -not -name 'readme.mark' -delete
+        find ../dynamic/files/   -not -name 'readme.mark' -delete
+        find ../dynamic/logs/    -not -name 'readme.mark' -delete
+        find ../dynamic/modules/ -not -name 'readme.mark' -delete
+        find ../dynamic/tmp/     -not -name 'readme.mark' -delete
+        cp /dev/null ../dynamic/data/data.sqlite
+        break;;
+    n ) exit;;
+    * ) echo 'Please answer "y" or "n".';;
   esac
 done
