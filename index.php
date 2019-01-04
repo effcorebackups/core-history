b1b843ff75ce2af699dbc7cd5dfb8b87839ad843
Maxim Rysevets <root@effcore.com>
2019-01-04 17:07:50 +0300

    Iteration #400-170

--- index.php
+++ index.php
@@ -4,14 +4,14 @@
   ### Copyright © 2017—2019 Maxim Rysevets. All rights reserved. ###
   ##################################################################
 
-  if (version_compare(phpversion(), '7.1.0', '<')) {
-    print 'Current version of PHP is: '.phpversion().'<br>';
-    print 'Required version is: 7.1.0+';
-  } else {
+  if (version_compare(PHP_VERSION, '7.1.0', '>=')) {
     $www_root = DIRECTORY_SEPARATOR == '\\' ? str_replace('\\', '/', __DIR__) : __DIR__;
     define('effcore\\dir_root',    $www_root.'/');
     define('effcore\\dir_dynamic', $www_root.'/dynamic/');
     define('effcore\\dir_system',  $www_root.'/system/');
     define('effcore\\dir_modules', $www_root.'/modules/');
     require_once('system/boot.php');
+  } else {
+    print 'The System requires a version of PHP not lower than 7.1.0<br>';
+    print 'The current version of PHP is '.PHP_VERSION;
   }
