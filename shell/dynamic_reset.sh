03b9e95e68176f73262fc3f49bba2f1622543d0c
Maxim Rysevets <root@effcore.com>
2019-01-07 23:58:19 +0300

    Iteration #400-182

--- shell/dynamic_reset.sh
+++ shell/dynamic_reset.sh
@@ -3,12 +3,12 @@
 while true; do
   read -p '!!!!!!!!!! ALL DATA WILL BE LOST !!!!!!!!!! Are you sure? [y/n]: ' answer
   case $answer in
-    y ) find ../dynamic/cache/   -not -name 'readme.mark' -delete
-        find ../dynamic/data/    -not -name 'readme.mark' -delete
-        find ../dynamic/files/   -not -name 'readme.mark' -delete
-        find ../dynamic/logs/    -not -name 'readme.mark' -delete
-        find ../dynamic/modules/ -not -name 'readme.mark' -delete
-        find ../dynamic/tmp/     -not -name 'readme.mark' -delete
+    y ) find ../dynamic/cache   -not -name 'readme.mark' -not -path '../dynamic/cache'   -delete
+        find ../dynamic/data    -not -name 'readme.mark' -not -path '../dynamic/data'    -delete
+        find ../dynamic/files   -not -name 'readme.mark' -not -path '../dynamic/files'   -delete
+        find ../dynamic/logs    -not -name 'readme.mark' -not -path '../dynamic/logs'    -delete
+        find ../dynamic/modules -not -name 'readme.mark' -not -path '../dynamic/modules' -delete
+        find ../dynamic/tmp     -not -name 'readme.mark' -not -path '../dynamic/tmp'     -delete
         cp /dev/null ../dynamic/data/data.sqlite
         break;;
     n ) exit;;
