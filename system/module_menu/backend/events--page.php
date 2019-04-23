800e0b3eda08e846d510e7cf0fe2df29cd6321e5
Maxim Rysevets <root@effcore.com>
2019-04-23 12:31:30 +0300

    Iteration #510-299

---
+++ system/module_menu/backend/events--page.php
@@ -0,0 +1,17 @@
+<?php
+
+  ##################################################################
+  ### Copyright © 2017—2019 Maxim Rysevets. All rights reserved. ###
+  ##################################################################
+
+namespace effcore\modules\menu {
+          use \effcore\tree;
+          abstract class events_page {
+
+  static function on_show_block_dynamic_tree($page, $args) {
+    if (!empty($args['id_tree'])) {
+      return tree::select($args['id_tree']);
+    }
+  }
+
+}}
