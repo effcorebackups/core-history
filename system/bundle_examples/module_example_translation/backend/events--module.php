e8bf9aec1ae67b255610407b4238b11a9aa80b0a
Maxim Rysevets <root@effcore.com>
2019-10-07 20:28:23 +0300

    Iteration #529-354

--- system/bundle_examples/module_example_translation/backend/events--module.php
+++ system/bundle_examples/module_example_translation/backend/events--module.php
@@ -5,12 +5,17 @@
   ######################################################################
 
 namespace effcore\modules\example_translation {
+          use \effcore\language;
+          use \effcore\message;
           use \effcore\module;
+          use \effcore\text;
           abstract class events_module {
 
   static function on_enable() {
     $module = module::get('example_translation');
     $module->enable();
+    message::insert(new text('Language %%_name was enabled.',                  ['name' => language::get('eo')->title->en]));
+    message::insert(new text('Translations for language %%_name was enabled.', ['name' => language::get('eo')->title->en]));
   }
 
   static function on_disable() {
