07ccbd64b4cb1de2b1fb11f71bf9cb1b08f6238e
Maxim Rysevets <root@effcore.com>
2019-10-12 22:51:30 +0300

    Iteration #529-387

--- system/module_translation_by/backend/events--module.php
+++ system/module_translation_by/backend/events--module.php
@@ -4,7 +4,7 @@
   ### Copyright © 2017—2020 Maxim Rysevets. All rights reserved. ###
   ##################################################################
 
-namespace effcore\modules\translation_by {
+namespace effcore\modules\translation_be {
           use \effcore\language;
           use \effcore\message;
           use \effcore\module;
@@ -14,15 +14,15 @@ namespace effcore\modules\translation_by {
           abstract class events_module {
 
   static function on_enable() {
-    $module = module::get('translation_by');
+    $module = module::get('translation_be');
     $module->enable();
     message::insert(
-      new text('You can enable or disable the language %%_name on page %%_page.', ['name' => language::get('by')->title_en, 'page' => translation::get('Locales')])
+      new text('You can enable or disable the language %%_name on page %%_page.', ['name' => language::get('be')->title_en, 'page' => translation::get('Locales')])
     );
   }
 
   static function on_disable() {
-    $module = module::get('translation_by');
+    $module = module::get('translation_be');
     $module->disable();
     storage::get('files')->changes_insert('locales', 'update', 'settings/locales/lang_code', 'en');
     language::code_set_current('en');
