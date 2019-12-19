8169fc479150a0df4a62c975bb34f41773176e17
Maxim Rysevets <root@effcore.com>
2019-12-19 15:55:33 +0300

    Iteration #1020-10

--- system/module_polls/backend/pattern--widget_poll_fields.php
+++ system/module_polls/backend/pattern--widget_poll_fields.php
@@ -9,7 +9,7 @@ namespace effcore {
 
   public $tag_name = 'x-widget';
   public $attributes = ['data-type' => 'poll_fields'];
-  public $fields_prefix = '';
+  public $unique_prefix = '';
 
   function __construct($attributes = [], $weight = 0) {
     parent::__construct(null, null, null, $attributes, [], $weight);
@@ -27,13 +27,13 @@ namespace effcore {
         $c_field_text = new field_text('Text');
         $c_field_text->description_state = 'hidden';
         $c_field_text->build();
-        $c_field_text->name_set($this->fields_prefix.'text_'.$i);
+        $c_field_text->name_set($this->unique_prefix.'text_'.$i);
         $c_field_text->required_set($i == 0 || $i == 1);
       # field for weight
         $c_field_weight = new field_weight();
         $c_field_weight->description_state = 'hidden';
         $c_field_weight->build();
-        $c_field_weight->name_set($this->fields_prefix.'weight_'.$i);
+        $c_field_weight->name_set($this->unique_prefix.'weight_'.$i);
         $c_field_weight->required_set(false);
         $c_field_weight->value_set($c_widget_manage_weight);
       # group the fields in widget 'manage'
@@ -44,7 +44,7 @@ namespace effcore {
         $widgets_group_manage->child_insert($c_widget_manage, 'manage_'.$i);}
       $button_insert = new button('insert', ['title' => new text('insert')]);
       $button_insert->build();
-      $button_insert->value_set($this->fields_prefix.'button_insert');
+      $button_insert->value_set($this->unique_prefix.'button_insert');
     # insert all widgets
       $this->child_insert($widgets_group_manage, 'manage');
       $this->child_insert($button_insert,        'insert');
@@ -56,12 +56,26 @@ namespace effcore {
   }
 
   function items_set($items) {
+    if ($this->cform->validation_cache_get($this->unique_prefix.'fields') === null) {
+        $this->cform->validation_cache_set($this->unique_prefix.'fields', $items);
+    }
   }
 
-  function on_click_insert($group, $form, $npath, $value) {
+  function on_click_insert($form, $npath) {
   }
 
-  function on_click_delete($group, $form, $npath) {
+  function on_click_delete($form, $npath) {
+  }
+
+  ###########################
+  ### static declarations ###
+  ###########################
+
+  static function on_submit(&$group, $form, $npath) {
+    $button = $group->child_select('insert');
+    if ($button->is_clicked()) {
+      $group->on_click_insert($form, $npath);
+    }
   }
 
 }}
