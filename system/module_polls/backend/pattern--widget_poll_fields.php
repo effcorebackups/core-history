3b9e2fd60b8c9bb2c8f0f5b055d171f1d1cc7248
Maxim Rysevets <root@effcore.com>
2020-01-15 16:18:57 +0300

    Iteration #1027-2

--- system/module_polls/backend/pattern--widget_poll_fields.php
+++ system/module_polls/backend/pattern--widget_poll_fields.php
@@ -10,6 +10,20 @@ namespace effcore {
   public $attributes = ['data-type' => 'poll_fields'];
   public $item_title = 'Answer';
 
+  function widget_manage_get($item, $c_row_id, $prefix) {
+    $widget = parent::widget_manage_get($item, $c_row_id, $prefix);
+  # field for text
+    $field_text = new field_text;
+    $field_text->description_state = 'hidden';
+    $field_text->build();
+    $field_text->name_set($prefix.'text'.$c_row_id);
+    $field_text->value_set($item->text);
+    $this->_fields['text'.$c_row_id] = $field_text;
+  # group the previous elements in widget 'manage'
+    $widget->child_insert($field_text, 'text');
+    return $widget;
+  }
+
   function on_cache_update($form, $npath) {
     $items = $this->items_get();
     foreach ($items as $c_row_id => $c_item) {
