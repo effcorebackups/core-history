b30e19757c5fe71e37d443850721ea61939409ff
Maxim Rysevets <root@effcore.com>
2020-06-12 12:14:49 +0300

    Iteration #1041-276

--- shell/repo_restore.sh
+++ shell/repo_restore.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
 
-cd ../
+repo_url=$(grep 'repo_url: ' ../system/bundle.data | sed 's/  repo_url: //g')
+repo_branch=$(grep 'repo_branch: ' ../system/bundle.data | sed 's/  repo_branch: //g')
 
-repo_url=$(grep 'repo_url: ' system/bundle.data | sed 's/  repo_url: //g')
-repo_branch=$(grep 'repo_branch: ' system/bundle.data | sed 's/  repo_branch: //g')
-
-rm -rf .git
-rm -rf dynamic/tmp/repo_system
-git clone --branch=$repo_branch $repo_url dynamic/tmp/repo_system
-mv dynamic/tmp/repo_system/.git .
-rm -rf dynamic/tmp/repo_system
+rm -rf ../.git
+rm -rf ../dynamic/tmp/repo_system
+git clone --branch=$repo_branch $repo_url ../dynamic/tmp/repo_system
+mv ../dynamic/tmp/repo_system/.git ../
+rm -rf ../dynamic/tmp/repo_system
