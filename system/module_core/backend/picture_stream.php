00cd39e8e7665c205c104ca2cc60ae676c389f06
Maxim Rysevets <root@effcore.com>
2020-12-23 21:00:24 +0300

    Iteration #1041-1262

--- system/module_core/backend/picture_stream.php
+++ system/module_core/backend/picture_stream.php
@@ -17,22 +17,30 @@ namespace effcore {
 
   static function path_parse($path) {
     $file = new file($path);
+    if ($file->is_path_absolute() === false) throw new \Exception('The path should be absolute.');
     if ($file->protocol === static::type) {
       $result = new \stdClass;
-      $result->container = [];
-      $result->enclosure = [];
+      $result->external = [];
+      $result->internal = [];
       $is_container_found = false;
       foreach ($file->dirs_get_parts() + ['file' => $file->file_get()] as $c_part) {
         $c_info = file::path_parse($c_part, true);
         if (!$c_info) return; # return if any component of path is broken
-        if ($is_container_found !== true) $result->container[] = $c_part;
-        if ($is_container_found === true) $result->enclosure[] = $c_part;
+        if ($is_container_found !== true) $result->external[] = $c_part;
+        if ($is_container_found === true) $result->internal[] = $c_part;
         if ($is_container_found !== true && $c_info->type === static::type && strlen($c_info->name)) $is_container_found = true;
       }
       if ($is_container_found) {
+        $result->path_phardata =          'phar:///'.implode('/', $result->external);
+        $result->path_external = static::type.':///'.implode('/', $result->external);
+        $result->path_internal =                     implode('/', $result->internal);
         return $result;
       }
     }
+  }
+
+  static function register() {
+    stream_wrapper_register(static::type, __CLASS__);
   }
 
 }}
