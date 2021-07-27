2eb2e740ddc46e3680ceb857a274b38092aafba1
Maxim Rysevets <root@effcore.com>
2021-07-27 13:13:10 +0300

    Iteration #1043-2

--- system/module_poll/backend/events--form-instance_insert.php
+++ system/module_poll/backend/events--form-instance_insert.php
@@ -4,14 +4,14 @@
   ### Copyright © 2017—2021 Maxim Rysevets. All rights reserved. ###
   ##################################################################
 
-namespace effcore\modules\polls {
+namespace effcore\modules\poll {
+          use \effcore\core;
           use \effcore\entity;
           use \effcore\instance;
-          use \effcore\page;
           use \effcore\text;
           use \effcore\url;
           use \effcore\widget_texts;
-          abstract class events_form_instance_update {
+          abstract class events_form_instance_insert {
 
   static function on_init($event, $form, $items) {
     if ($form->has_error_on_init === false) {
@@ -19,26 +19,17 @@ namespace effcore\modules\polls {
       if ($entity) {
         if ($entity->name === 'poll') {
           $form->is_redirect_enabled = false;
-          $form->_answers_rows = entity::get('poll_answer')->instances_select(['conditions' => [
-            'id_poll_!f'       => 'id_poll',
-            'id_poll_operator' => '=',
-            'id_poll_!v'       => $form->_instance->id]], 'id');
-          $widget_items = [];
-          foreach ($form->_answers_rows as $c_row) {
-            $widget_items[] = (object)[
-              'id'     => $c_row->id,
-              'weight' => $c_row->weight,
-              'text'   => $c_row->answer
-            ];
-          }
+          $items['#expired']->value_set(core::datetime_get('+'.core::date_period_w.' second'));
           $widget_answers = new widget_texts;
           $widget_answers->title = 'Answers';
           $widget_answers->item_title = 'Answer';
           $widget_answers->name_complex = 'widget_answers';
           $widget_answers->cform = $form;
-          $widget_answers->weight = 140;
+          $widget_answers->weight = -500;
           $widget_answers->build();
-          $widget_answers->value_set_complex($widget_items, true);
+          $widget_answers->value_set_complex([
+            (object)['weight' =>  0, 'id' => 0, 'text' => 'Answer 1'],
+            (object)['weight' => -5, 'id' => 0, 'text' => 'Answer 2']], true);
           $form->child_select('fields')->child_insert($widget_answers, 'answers');
         }
       }
@@ -49,8 +40,9 @@ namespace effcore\modules\polls {
     $entity = entity::get($form->entity_name);
     if ($entity) {
       switch ($form->clicked_button->value_get()) {
-        case 'update':
-          if ($entity->name === 'poll') {
+        case 'insert':
+        case 'insert_and_update':
+          if ($entity->name === 'poll' && !empty($form->_instance)) {
             if (count($items['*widget_answers']->value_get_complex()) < 2) {
               $form->error_set('Group "%%_title" should contain a minimum %%_number item%%_plural{number|s}!', ['title' => (new text($items['*widget_answers']->title))->render(), 'number' => 2]);
             }
@@ -63,36 +55,18 @@ namespace effcore\modules\polls {
   static function on_submit($event, $form, $items) {
     $entity = entity::get($form->entity_name);
     if ($entity) {
-      if ($entity->name === 'poll') {
+      if ($entity->name === 'poll' && !empty($form->_instance)) {
         switch ($form->clicked_button->value_get()) {
-          case 'update':
-            $used_ids = [];
+          case 'insert':
+          case 'insert_and_update':
             foreach ($items['*widget_answers']->value_get_complex() as $c_item) {
-            # insert new answer
-              if ($c_item->id == 0) {
-                (new instance('poll_answer', [
-                  'id_poll' => $form->_instance->id,
-                  'answer'  => $c_item->text,
-                  'weight'  => $c_item->weight
-                ]))->insert();
-              }
-            # update current answer
-              if ($c_item->id != 0) {
-                $form->_answers_rows[$c_item->id]->answer = $c_item->text;
-                $form->_answers_rows[$c_item->id]->weight = $c_item->weight;
-                $form->_answers_rows[$c_item->id]->update();
-                $used_ids[$c_item->id] =
-                          $c_item->id;
-              }
+              (new instance('poll_answer', [
+                'id_poll' => $form->_instance->id,
+                'answer'  => $c_item->text,
+                'weight'  => $c_item->weight
+              ]))->insert();
             }
-          # delete old answers
-            foreach ($form->_answers_rows as $c_row) {
-              if (!isset($used_ids[$c_row->id])) {
-                $c_row->delete();
-              }
-            }
-          # reset not actual data (for load new IDs too)
-            $form->_answers_rows = null;
+          # reset not actual data
             $items['*widget_answers']->items_reset();
             static::on_init(null, $form, $items);
           # ↓↓↓ no break ↓↓↓
--- system/module_poll/backend/events--form-instance_update.php
+++ system/module_poll/backend/events--form-instance_update.php
@@ -4,14 +4,14 @@
   ### Copyright © 2017—2021 Maxim Rysevets. All rights reserved. ###
   ##################################################################
 
-namespace effcore\modules\polls {
-          use \effcore\core;
+namespace effcore\modules\poll {
           use \effcore\entity;
           use \effcore\instance;
+          use \effcore\page;
           use \effcore\text;
           use \effcore\url;
           use \effcore\widget_texts;
-          abstract class events_form_instance_insert {
+          abstract class events_form_instance_update {
 
   static function on_init($event, $form, $items) {
     if ($form->has_error_on_init === false) {
@@ -19,17 +19,26 @@ namespace effcore\modules\polls {
       if ($entity) {
         if ($entity->name === 'poll') {
           $form->is_redirect_enabled = false;
-          $items['#expired']->value_set(core::datetime_get('+'.core::date_period_w.' second'));
+          $form->_answers_rows = entity::get('poll_answer')->instances_select(['conditions' => [
+            'id_poll_!f'       => 'id_poll',
+            'id_poll_operator' => '=',
+            'id_poll_!v'       => $form->_instance->id]], 'id');
+          $widget_items = [];
+          foreach ($form->_answers_rows as $c_row) {
+            $widget_items[] = (object)[
+              'id'     => $c_row->id,
+              'weight' => $c_row->weight,
+              'text'   => $c_row->answer
+            ];
+          }
           $widget_answers = new widget_texts;
           $widget_answers->title = 'Answers';
           $widget_answers->item_title = 'Answer';
           $widget_answers->name_complex = 'widget_answers';
           $widget_answers->cform = $form;
-          $widget_answers->weight = -500;
+          $widget_answers->weight = 140;
           $widget_answers->build();
-          $widget_answers->value_set_complex([
-            (object)['weight' =>  0, 'id' => 0, 'text' => 'Answer 1'],
-            (object)['weight' => -5, 'id' => 0, 'text' => 'Answer 2']], true);
+          $widget_answers->value_set_complex($widget_items, true);
           $form->child_select('fields')->child_insert($widget_answers, 'answers');
         }
       }
@@ -40,9 +49,8 @@ namespace effcore\modules\polls {
     $entity = entity::get($form->entity_name);
     if ($entity) {
       switch ($form->clicked_button->value_get()) {
-        case 'insert':
-        case 'insert_and_update':
-          if ($entity->name === 'poll' && !empty($form->_instance)) {
+        case 'update':
+          if ($entity->name === 'poll') {
             if (count($items['*widget_answers']->value_get_complex()) < 2) {
               $form->error_set('Group "%%_title" should contain a minimum %%_number item%%_plural{number|s}!', ['title' => (new text($items['*widget_answers']->title))->render(), 'number' => 2]);
             }
@@ -55,18 +63,36 @@ namespace effcore\modules\polls {
   static function on_submit($event, $form, $items) {
     $entity = entity::get($form->entity_name);
     if ($entity) {
-      if ($entity->name === 'poll' && !empty($form->_instance)) {
+      if ($entity->name === 'poll') {
         switch ($form->clicked_button->value_get()) {
-          case 'insert':
-          case 'insert_and_update':
+          case 'update':
+            $used_ids = [];
             foreach ($items['*widget_answers']->value_get_complex() as $c_item) {
-              (new instance('poll_answer', [
-                'id_poll' => $form->_instance->id,
-                'answer'  => $c_item->text,
-                'weight'  => $c_item->weight
-              ]))->insert();
+            # insert new answer
+              if ($c_item->id == 0) {
+                (new instance('poll_answer', [
+                  'id_poll' => $form->_instance->id,
+                  'answer'  => $c_item->text,
+                  'weight'  => $c_item->weight
+                ]))->insert();
+              }
+            # update current answer
+              if ($c_item->id != 0) {
+                $form->_answers_rows[$c_item->id]->answer = $c_item->text;
+                $form->_answers_rows[$c_item->id]->weight = $c_item->weight;
+                $form->_answers_rows[$c_item->id]->update();
+                $used_ids[$c_item->id] =
+                          $c_item->id;
+              }
             }
-          # reset not actual data
+          # delete old answers
+            foreach ($form->_answers_rows as $c_row) {
+              if (!isset($used_ids[$c_row->id])) {
+                $c_row->delete();
+              }
+            }
+          # reset not actual data (for load new IDs too)
+            $form->_answers_rows = null;
             $items['*widget_answers']->items_reset();
             static::on_init(null, $form, $items);
           # ↓↓↓ no break ↓↓↓
